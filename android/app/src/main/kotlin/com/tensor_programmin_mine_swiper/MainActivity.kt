d3aa95446c1c016eb033e3b9cadef9a9a521feb9
vladimir <babenkovladimirbmd@gmail.com>
2019-03-03 11:43:43 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/tensor_programmin_mine_swiper/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.tensor_programmin_mine_swiper
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
